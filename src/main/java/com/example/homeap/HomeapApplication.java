4dad1398f99ba169ff6fdff277c84a613f72b077
ehowl9509 <ehowl9509@naver.com>
2020-07-15 23:34:17 +0900

    인텔리제이 git 연동 테스트

---
+++ src/main/java/com/example/homeap/HomeapApplication.java
@@ -0,0 +1,13 @@
+package com.example.homeap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeapApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/homeap/HomeapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.homeap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
